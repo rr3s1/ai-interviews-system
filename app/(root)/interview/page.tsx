85942af1c66bd021f74faa3e241567d2120be5fc
aessary <raressilviulazar@gmail.com>
2025-07-02 22:12:11 +0100

    Vapi Workflow & Assistan2

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Agent from '@/components/Agent'
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Page = () => {
+const Page = async () => {
+    const user = await getCurrentUser();
+
     return (
         <>
             <h3>Interview Generation</h3>
 
+
             <Agent
-                userName="RESER"
-                userId="{user?.id}"
-                // profileImage="{user?.profileURL}"
+                userName={user?.name!}
+                userId={user?.id}
+                profileImage={user?.profileURL}
                 type="generate"
             />
         </>
